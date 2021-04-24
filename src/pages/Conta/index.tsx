523478b553ea3e32fa6a0aa5da7654b6752c37e8
Rubens Diego <rdsdo2011@gmail.com>
2021-04-23 23:00:15 -0300

    Refatorando

--- src/pages/Conta/index.tsx
+++ src/pages/Conta/index.tsx
@@ -1,46 +1,94 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from '../../component/buttons/Button'
 import InputCadastro from '../../component/inputs/InputCadastro'
 import LayoutPrincipal from '../../component/LayoutPrincipal'
 import { CardListTab, Tab, Tabs } from '../../component/TabsComponents'
 import CardRegisterTab from '../../component/TabsComponents/CardRegisterTab'
+import { api } from '../../services/api'
+
+
+
+interface IContas {
+
+    id: number
+    qtdPontosUsados: number
+    contadorMovimento?: number
+    ativo: boolean
+    bloqueado: boolean
+    nomeConta: string
+    qtdPontos: number
+    valorConta: number
+    usuariosIdFK?: {
+        id: number
+    },
+    bancosIdFK?: {
+        id: number
+    },
+
+}
+
+const Conta: React.FC = () => {
+
+
+    const [listContas, setListContas] = useState<IContas[]>([])
+    const auth = localStorage.getItem('Authorization')
+
+
+
+    useEffect(() => {
+
+        api.get('conta',
+            { headers: { authorization: auth } })
+            .then(response => {
+                const resposta: any = response.data
+
+
+                
+                setListContas(resposta)
+
+
+            })
+            .catch(erro => {
+                alert('Erro ao acessar servidor!')
+
+            })
+
+    }, [])
 
 
 
 
- const Conta: React.FC = () => {
+    return (
+        <LayoutPrincipal titulo="Conta" >
 
-return(
-<LayoutPrincipal titulo="Conta" >
-   
-<Tab>
-    <Tabs
-    text='Cadastro'
-    IdNameTab= "tabRegisterConta"
-    defaultCheckedTab    >
-        <CardRegisterTab>
-            <InputCadastro>Banco</InputCadastro>
-            <InputCadastro>Nome da Conta</InputCadastro>
-            <InputCadastro>Pontos</InputCadastro>
-            <InputCadastro>Valor Inicial</InputCadastro>
-          
-     
-    <Button>Cadstrar</Button>
-        </CardRegisterTab>
-      
-    </Tabs>
-
-    <Tabs
-    text='Lista de contas'
-    IdNameTab= "tabListConta" >
-        <CardListTab></CardListTab>
-    </Tabs>
-    
-</Tab>
+            <Tab>
+                <Tabs
+                    text='Cadastro'
+                    IdNameTab="tabRegisterConta"
+                    defaultCheckedTab    >
+                    <CardRegisterTab>
+                        <InputCadastro>Banco</InputCadastro>
+                        <InputCadastro>Nome da Conta</InputCadastro>
+                        <InputCadastro>Pontos</InputCadastro>
+                        <InputCadastro>Valor Inicial</InputCadastro>
 
 
-   </LayoutPrincipal>
+                        <Button>Cadstrar</Button>
+                    </CardRegisterTab>
 
-)
+                </Tabs>
+
+                <Tabs
+                    text='Lista de contas'
+                    IdNameTab="tabListConta" >
+                    <CardListTab></CardListTab>
+                </Tabs>
+
+            </Tab>
+
+
+        </LayoutPrincipal>
+
+    )
 }
 export default Conta
