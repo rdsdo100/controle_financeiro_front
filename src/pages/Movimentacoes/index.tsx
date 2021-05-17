e643a53e390009ca1ca6371676919decad9c9d8f
Rubens Diego <rdsdo2011@gmail.com>
2021-05-17 11:34:59 -0300

    Deletando linhas em branco

--- src/pages/Movimentacoes/index.tsx
+++ src/pages/Movimentacoes/index.tsx
@@ -1,39 +1,11 @@
 import React from 'react'
 import LayoutPrincipal from '../../component/LayoutPrincipal'
-import { CardListTab, Tab, Tabs } from '../../component/TabsComponents'
-import CardRegisterTab from '../../component/TabsComponents/CardRegisterTab'
-
-
-
 
 const Movimentacoes: React.FC = () => {
-
-
    
     return (
         <LayoutPrincipal titulo="Conta" >
 
-            <Tab>
-
-                <Tabs
-                    text='Lista de contas'
-                    IdNameTab="tabListMovimentacoes"
-                    defaultCheckedTab  >
-                    <CardListTab>
-                    
-                    </CardListTab>
-                </Tabs>
-
-                <Tabs
-                    text='Cadastro'
-                    IdNameTab="tabRegisterMovimentacoes"
-                >
-                    <CardRegisterTab>
-
-                    </CardRegisterTab>
-
-                </Tabs>
-            </Tab>
 
 
         </LayoutPrincipal>
