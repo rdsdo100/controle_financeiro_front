4f43e4c7942c72b7bd959b357a98382a25a376f3
Rubens Diego <rdsdo2011@gmail.com>
2021-06-03 23:50:10 -0300

    Refatorando

--- src/component/cards/CardsMovimentacoes/styles.ts
+++ src/component/cards/CardsMovimentacoes/styles.ts
@@ -13,12 +13,26 @@ export const Container = styled.div`
 position: relative;
 height: 150px;
 width: calc(5px -100%);
-display: flex;
 box-sizing: border-box;
+border: 1px solid  white;
 border-radius: 5px;
 background-color: ${props => props.theme.colors.tertiary};
   box-shadow: 0 15px 25px rgba(0,0,0,.6);
   margin: 10px 10px 5px 5px;
+
+  display: grid;
+  grid-template-rows:  90% auto;
+    grid-template-columns: auto 1200px auto ;
+
+    background-color:${props => props.theme.colors.primary};
+    grid-template-areas:
+    "MH MH MH" 
+    "ATL PR ATR";
+    grid-gap: 5px;
+
+
+
+
 
   &:hover {
         background-color: ${props => props.theme.colors.primary};
