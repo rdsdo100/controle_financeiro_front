194e7a8e9f97a6a85a83439d8197dc3cf9227b16
Rubens Diego <rdsdo2011@gmail.com>
2021-07-11 22:44:32 -0300

    Refatorando pagina de Login

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
+import Login from "../pages/Login";
 import User from "../pages/User"
 import PrivateRoute from "./privateRoutes";
 
@@ -10,8 +11,9 @@ const Routes = () => {
   return (
     <BrowserRouter>
      
-        <Route component={Home} path='/' exact ></Route>
-        <PrivateRoute component={User} path='user' exact ></PrivateRoute>
+        <Route component={Login} path='/' exact ></Route>
+        <Route component={Home} path='/home'  ></Route>
+        <PrivateRoute component={User} path='/user'  ></PrivateRoute>
 
         
         
