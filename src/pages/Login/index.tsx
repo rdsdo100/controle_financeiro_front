32f075790320b97de5b118042ed4db0d15fa9817
Rubens Diego <rdsdo2011@gmail.com>
2021-07-15 13:42:49 -0300

    Refatorando

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,16 +1,13 @@
-import React from 'react'
-
+import React from "react";
+import { Component , LoginBox } from "./styles";
 
 const Login: React.FC = () => {
-
-   
-return(
-    <div>
-
-
-    </div>
-)
-}
-export default Login
+  return (
+    <Component>
+      <LoginBox></LoginBox>
+    </Component>
+  );
+};
+export default Login;
 
 //https://mariosouto.com/inputs-materializados-com-css/
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 
@@ -13,7 +12,8 @@ export const LoginBox = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
-  width: 400px;
+  width: 700px;
+  height: 500px;
   padding: 40px;
   transform: translate(-50%, -50%);
   background: linear-gradient( ${props => props.theme.colors.secundary} , ${props => props.theme.colors.primary});
